9a4a0cd9392fd5de9c6050f9facfd09e29ef9810
unknown <abaag@uptycs.com>
2023-01-29 12:56:55 +0530

    added vulnerability code for python

--- gittest/sample.py
+++ gittest/sample.py
@@ -3,3 +3,9 @@ import sys
 
 print("sample python")
 print(os.path.abspath(__file__))
+
+os.system('dir')
+
+input_location = 'C:\uptycs\githubquery\gitquery_arun\gittest\sample.py'
+split_list = os.path.split(input_location)
+print("split list: {}".format(split_list))
